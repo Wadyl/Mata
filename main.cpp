d762d41c8e09e2b44841ea9560ee4203fb42a342
Wadyl <75365274+Wadyl@users.noreply.github.com>
2020-12-02 11:23:01 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <random>
+#include <iostream>
+
+using namespace std;
+int liczba;
+int main(){
+    random_device losowa;
+    default_random_engine gen(losowa());
+    uniform_int_distribution<int> przedzial( 1 , 6 );
+    liczba = przedzial(gen);
+
+    cout << liczba << endl;
+    return  liczba;
+}
